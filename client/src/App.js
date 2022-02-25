b39af6b47f2293010a5c00628a19d937f78aefcf
ChandrakumaranSubiharan <wordpress.subiharan@gmail.com>
2022-02-25 11:08:54 +0530

    login selection ui commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,32 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import NavBar from './components/NavBar';
-import Home from './components/Home';
+import LoginSelection from './components/LoginSelection';
 import AdminPage from './components/admin-page/AdminPage';
 import LoginPage from './components/Login';
 import RegisterPage from './components/Register';
 import DriverPage from './components/driver-page/DriverPage';
 import UserPage from './components/user-page/UserPage';
 
+
 function App() {
   return (
-      <Router>
-        <NavBar />
-        <Switch>
-            <Route path="/" exact component = {Home} />
-            <Route path="/admin-page" component = {AdminPage} />
-            <Route path="/login-page" component = {LoginPage} />
-            <Route path="/register-page" component = {RegisterPage} />
-            <Route path="/driver-page" component = {DriverPage} />
-            <Route path="/user-page" component = {UserPage} />
-        </Switch>
-      </Router>
+
+    <LoginSelection />
+    // <LoginPage />
+
+      // <Router>
+      //   <NavBar />
+      //   <Switch>
+      //       <Route path="/" exact component = {Home} />
+      //       <Route path="/admin-page" component = {AdminPage} />
+      //       <Route path="/login-page" component = {LoginPage} />
+      //       <Route path="/register-page" component = {RegisterPage} />
+      //       <Route path="/driver-page" component = {DriverPage} />
+      //       <Route path="/user-page" component = {UserPage} />
+      //   </Switch>
+      // </Router>
+
   );
 }
 
